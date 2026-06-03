eb34fe4bb7735394c00c7ef3f3d696afcf91d195
John Bush <jbush.freelance@gmail.com>
2026-06-03 04:44:56 -0700

    added TestVessel class

--- read_data/TestOrbitInfo.java
+++ read_data/TestOrbitInfo.java
@@ -9,11 +9,26 @@ class TestOrbitInfo {
             if(oi == null) {
                 break;
             }
+            TestVessel foo = new TestVessel(oi);
+
+            
             System.out.println("orbit id: " + (oi.getId()));
             System.out.println("object name: " + oi.getName());
             Orbit o = oi.makeOrbit();
             o.printFields();
             System.out.println();
+            printStateVector(foo.randStateVector());
+            System.out.println();
         }
     }
+
+    private static void printStateVector(double[] v) {
+        System.out.println("Vector: ");
+        System.out.print("[ ");
+        for(int i = 0; i < 5; i++) {
+            System.out.printf("%4.4f, ", v[i]);
+        }
+        System.out.printf("%4.4f ]\n\n", v[5]);
+    }
+
 }
